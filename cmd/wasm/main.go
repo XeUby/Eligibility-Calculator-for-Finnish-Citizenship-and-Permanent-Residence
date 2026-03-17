73933b81ec610af060f01e96646cce9fc9cb8349
Boris Chugin <boris.chugin@gmail.com>
2026-03-18 00:04:40 +0200

    Comprehensive update: detailed info cards and betteer UI

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,44 +1,103 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"syscall/js"
 	"time"
-
-	"github.com/XeUby/Eligibility-Calculator-for-Finnish-Citizenship-and-Permanent-Residence/internal/calculator"
-	"github.com/XeUby/Eligibility-Calculator-for-Finnish-Citizenship-and-Permanent-Residence/internal/models"
 )
 
 func calculateWrapper(this js.Value, args []js.Value) interface{} {
-	// Получаем массив пермитов из JS
 	jsPermits := args[0]
-	abroadDays := args[1].Int()
+	abroadTotal := args[1].Int()
+	abroad12m := args[2].Int()
 
-	var permits []models.Permit
+	var citizenshipDays float64 = 0
+	var bCredited float64 = 0
+	var aCredited float64 = 0
+
 	layout := "2006-01-02"
+	var lastDate time.Time
+	var firstADate time.Time
 
 	for i := 0; i < jsPermits.Length(); i++ {
 		p := jsPermits.Index(i)
-		start, _ := time.Parse(layout, p.Get("start").String())
-		end, _ := time.Parse(layout, p.Get("end").String())
-		permits = append(permits, models.Permit{
-			Type:      p.Get("type").String(),
-			StartDate: start,
-			EndDate:   end,
-		})
+		startStr := p.Get("start").String()
+		endStr := p.Get("end").String()
+		pType := p.Get("type").String()
+
+		if startStr == "" || endStr == "" {
+			continue
+		}
+
+		start, _ := time.Parse(layout, startStr)
+		end, _ := time.Parse(layout, endStr)
+
+		if end.After(lastDate) {
+			lastDate = end
+		}
+
+		// Включаем оба дня в расчет (+1)
+		days := math.Round(end.Sub(start).Hours()/24) + 1
+
+		if pType == "B" {
+			credited := days / 2.0
+			bCredited += credited
+			citizenshipDays += credited
+		} else if pType == "A" {
+			if firstADate.IsZero() || start.Before(firstADate) {
+				firstADate = start
+			}
+			aCredited += days
+			citizenshipDays += days
+		}
 	}
 
-	totalDays := calculator.CalculateResidence(permits)
-	// Вычитаем дни за границей
-	effectiveDays := totalDays - float64(abroadDays)
+	// Логика Absence
+	totalLeft := 365 - abroadTotal
+	last12mLeft := 90 - abroad12m
+	postponesBy := 0
 
-	eligible, msg := calculator.CheckEligibility(effectiveDays)
+	statusReason := fmt.Sprintf("OK: within limits (<=365 total, <=90 last 12m). Total left: %d | Last 12m left: %d", totalLeft, last12mLeft)
 
-	return map[string]interface{}{
-		"total_days":  effectiveDays,
-		"is_eligible": eligible,
-		"message":     msg,
-		"days_from_b": totalDays - effectiveDays, // для справки
+	if totalLeft < 0 || last12mLeft < 0 {
+		statusReason = "Warning: Limits exceeded. Continuous residence may be broken."
+		if totalLeft < 0 {
+			postponesBy += int(math.Abs(float64(totalLeft)))
+		}
+		if last12mLeft < 0 {
+			postponesBy += int(math.Abs(float64(last12mLeft)))
+		}
 	}
+
+	// Citizenship (5 years / 1825 days)
+	citReq := 1825.0
+	isCitEligible := citizenshipDays >= citReq && postponesBy == 0
+	citApplyDate := "Please enter dates"
+
+	if isCitEligible {
+		citApplyDate = "Eligible to apply now! 🎉"
+	} else if !lastDate.IsZero() {
+		daysNeeded := int(citReq-citizenshipDays) + postponesBy
+		citApplyDate = lastDate.AddDate(0, 0, daysNeeded).Format("02/01/2006")
+	}
+
+	// PR (4 years / 1460 days - ONLY A type)
+	prApplyDate := "N/A (Needs A-permit)"
+	if !firstADate.IsZero() {
+		prApplyDate = firstADate.AddDate(4, 0, 0).Format("02/01/2006")
+	}
+
+	return js.ValueOf(map[string]interface{}{
+		"cit_days":       citizenshipDays,
+		"b_credited":     bCredited,
+		"a_credited":     aCredited,
+		"cit_eligible":   isCitEligible,
+		"cit_apply_date": citApplyDate,
+		"status_reason":  statusReason,
+		"postpones_by":   postponesBy,
+		"pr_apply_date":  prApplyDate,
+	})
 }
 
 func main() {
